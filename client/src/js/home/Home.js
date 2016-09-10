d3ba83305e8a43a9ecc4809d0c9c72837d3f885a
wagoid <w.rsantos@outlook.com>
2016-09-10 01:07:05 -0300

    remove useless stuff from Home

--- client/src/js/home/Home.js
+++ client/src/js/home/Home.js
@@ -15,29 +15,7 @@ class Home extends Component {
 	}
 
 	render() {
-		return (
-			<section className="container home">
-				<form
-					className="form-inline"
-					role="form"
-					onSubmit={this.handleSubmit}
-				>
-					<div className="form-group">
-						<div className="input-group">
-							<input
-								type="text"
-								placeholder="Enter a GitHub user..."
-								className="form-control"
-								ref={ref => (this._input = ref)}
-							/>
-						</div>
-					</div>
-					<button type="submit" className="btn btn-primary">
-						Go
-					</button>
-				</form>
-			</section>
-		);
+		return null;
 	}
 }
 
