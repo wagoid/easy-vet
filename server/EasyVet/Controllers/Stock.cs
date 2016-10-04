290d6859c6bb7eca2a063e205ee8fb978bc486e2
ArthurVilaca <vilaca.arthur@yahoo.com.br>
2016-10-04 18:31:30 -0300

    start of stock controller

---
+++ server/EasyVet/Controllers/Stock.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EasyVet.Enumerations;
+using System.Web.Http;
+using EasyVet.Helpers;
+using System.Data.Entity;
+
+namespace EasyVet.Controllers
+{
+    public class Stock : Generic.Base
+    {
+
+        private DAO.Stock stock;
+
+        public Stock()
+            : base()
+        {
+            stock = new DAO.Stock(context);
+        }
+        public Stock(DAO.Interfaces.VetContext context)
+            : base(context)
+        {
+            this.context = context;
+            stock = new DAO.Stock(this.context);
+        }
+
+
+        #region Stock HTTP methods
+
+        [Route("api/stock")]
+        [HttpGet]
+        public Response<List<Models.Stock>> AllProducts()
+        {
+            return safelyRespond(() => stock.List());
+        }
+
+        [Route("api/stock")]
+        [HttpPost]
+        public Response<Models.Stock> PostProduct([FromBody]Models.Stock stock)
+        {
+            return safelyRespond(() => this.stock.Insert(stock));
+        }
+
+        [Route("api/stock")]
+        [HttpPut]
+        public Response<bool> PutProduct([FromBody]Models.Stock stock)
+        {
+            return safelyRespond(() => this.stock.Update(stock));
+        }
+
+        [Route("api/stock/{id}")]
+        [HttpDelete]
+        public Response<bool> DeleteProduct(int id)
+        {
+            return safelyRespond(() => stock.Delete(id));
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ server/EasyVet/DAO/Stock.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasyVet.DAO
+{
+    public class Stock : Generic.Base
+    {
+        public Stock() : base()
+        {
+
+        }
+
+        public Stock(Interfaces.VetContext context) : base(context)
+        {
+
+        }
+
+        public List<Models.Stock> List()
+        {
+            return getList(this.context.Stocks);
+        }
+
+        public Models.Stock FindById(int id)
+        {
+            return getFirstOrDefault(this.context.Stocks, id);
+        }
+
+        public Models.Stock Insert(Models.Stock stocks)
+        {
+            return post(context.Stocks, stocks);
+        }
+
+        public bool Update(Models.Stock stock)
+        {
+            return put(this.context.Stocks, stock);
+        }
+
+        public bool Delete(int id)
+        {
+            return delete(context.Stocks, id);
+        }
+    }
+}
